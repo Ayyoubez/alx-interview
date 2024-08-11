8af9b458337de404d56c48a2ad60748112b53250
ayyoubez <ayyoubez@gmail.com>
2024-08-11 23:14:54 +0100

    minoperation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module minoperations"""
+
+
+def minOperations(n):
+    """
+    minOperations
+    Get operations needed to result in exactly n H characters
+    """
+
+    if (n < 2):
+        return 0
+    op, root = 0, 2
+    while root <= n:
+        
+        if n % root == 0:
+            
+            op += root
+            
+            n = n / root
+            
+            root -= 1
+        
+        root += 1
+    return op
