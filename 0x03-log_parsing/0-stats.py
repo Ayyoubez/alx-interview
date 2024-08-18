7c566c9a174785ecb794196f854da08395e685df
ayyoubez <ayyoubez@gmail.com>
2024-08-19 00:12:47 +0100

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,10 +7,16 @@ import datetime
 
 for i in range(10000):
     sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
+    # Define the log entry format
+    ip_format = "{:d}.{:d}.{:d}.{:d}"
+    status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    # Create the log entry
+    log_entry = (
+        f"{ip_format.format(random.randint(1, 255), random.randint(1, 255), "
+        f"{random.randint(1, 255)}, {random.randint(1, 255)}) - "
+        f"[{datetime.datetime.now()}] \"GET /projects/260 HTTP/1.1\" "
+        f"{random.choice(status_codes)} {random.randint(1, 1024)}\n"
+    )
+    # Write the log entry to stdout
+    sys.stdout.write(log_entry)
     sys.stdout.flush()
